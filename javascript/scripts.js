3df1ab03b8b9c5b32bdfbe4a29b485b28e57d0f0
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-10 12:50:46 -0300

    Incio do estudo de Javascript

---
+++ javascript/scripts.js
@@ -0,0 +1,43 @@
+
+     console.log('Inicio do estudo de Js')
+
+console.log("Bem Vindo")
+console.log("criacao do index")
+console.log("Adição dos scripts")
+console.log("Continuacao do estudo de Js")
+console.log("Bom Dia")
+console.log("Teste")
+console.log("1+1 e")
+console.log(1+1)
+console.log(true)
+console.log("ou")
+console.log(false)
+console.log("A resposta certa e")
+console.log(true)
+console.log("Agora se")
+console.log("1+1 e")
+console.log(1+2)
+console.log(true)
+console.log("ou")
+console.log(false)
+console.log("A resposta certa e")
+console.log(false)
+console.log("Continuacao")
+console.log("Oi")
+console.log("adicao de objeto")
+console.log({
+  name: "Moises",
+  idade: "17",
+  altura: "1,80",
+  andar: function() {
+    console.log('andar')
+  }
+})
+console.log(["Moises, 17"])
+console.log([
+  "Moises",
+  "17",
+  "1,80",
+  "Andar",
+  "Comer"
+])
