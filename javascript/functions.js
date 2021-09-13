3608523a45934b6571f945d2c12cc99d4451b008
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-13 11:00:54 -0300

    Aula de Functions

---
+++ javascript/functions.js
@@ -0,0 +1,11 @@
+console.log('Aprendizado de functions')
+
+function createphrases() {
+    console.log('O dia esta otimo')
+    console.log('Esta muito quente')
+    console.log('Tenho sede')
+}
+
+createphrases()
+
+console.log('Fim')
