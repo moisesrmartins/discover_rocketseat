d73cf201879b98e6e462ad91d6e4b570ffb98b46
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-13 12:08:32 -0300

    Estudo sobre Function

---
+++ javascript/functions/functions.js
@@ -0,0 +1,9 @@
+console.log('Aprendizado de functions')
+
+function createphrases() {
+    console.log('O dia esta otimo')
+    console.log('Esta muito quente')
+    console.log('Tenho sede')
+}
+
+createphrases()
