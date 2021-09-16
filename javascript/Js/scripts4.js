54aa6356a46c968e47d2063b3eb1bb5eb483c9e6
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-16 12:29:23 -0300

    Inicio do estudo de Expressoes e Operadores

---
+++ javascript/Js/scripts4.js
@@ -0,0 +1,55 @@
+console.log('Estudo de Expressoes e Operadores')
+
+let number1
+
+(function(){
+  console.log('oi')
+})()
+
+//Se for acrescentado um '=' e um numero qualquer como '1'
+//'Function' ficara invalido e fara com que o programa falhe
+
+let number3 = 1
+
+console.log(++number3)
+
+let number4 = 1
+
+console.log(number4 + 2)
+console.log(true ? 'tchau' : 'nada')
+
+console.log('New')
+
+let name1= new String('Moises')
+let age1= new Number('17')
+let date1= new Date('2003-09-22')
+console.log(name)
+console.log(age1) 
+console.log(date1)
+
+console.log('Aplicacao do Delete')
+
+console.log(typeof 'Moises')
+const person1= {
+    name2: 'Moises',
+    age2: 17,
+    date2: '2003-09-21'
+}
+console.log(person1)
+
+const person2= {
+    name3: 'Moises',
+    age3: 17,
+    date3: '2003-09-21'
+}
+delete person2.date3
+console.log(person2)
+
+const person3= {
+    name4: 'Moises',
+    age4: 17,
+    date4: '2003-09-21'
+}
+delete person3.date4
+delete person3.age4
+console.log(person3)
