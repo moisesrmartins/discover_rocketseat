7cbe7ef815d7397b05621d8163de82d522192a4e
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-20 12:26:02 -0300

    Melhoria no Sistema de notas

--- javascript/Js/scripts7.js
+++ javascript/Js/scripts7.js
@@ -14,35 +14,40 @@ console.log('sistema de notas escolares')
 
 //Menor que 50 = F:
 
-let score= 100
-let scoreA= score >= 90 && score <= 100
+function getScore(score){
+    let scoreA= score >= 90 && score <= 100
+    let scoreB= score >= 80 && score <= 89
+    let scoreC= score >= 70 && score <= 79
+    let scoreD= score >= 60 && score <= 69
+    let scoreE= score >= 50 && score <= 59
+    let scoreF= score < 50 && score >= 0
+    
+    let scoreFinal;
 
-let scoreB= score >= 80 && score <= 89
+    if(scoreA){
+        scoreFinal= 'A'
+    }else if(scoreB){
+        scoreFinal= 'B'
+    }else if(scoreC){
+        scoreFinal= 'C'
+    }else if(scoreD){
+        scoreFinal= 'D'
+    }else if(scoreE){
+        scoreFinal= 'E'
+    }else if(scoreF){
+        scoreFinal= 'F'
+    }else{
+        scoreFinal= 'Nota Invalida'
+    }
 
-let scoreC= score >= 70 && score <= 79
-
-let scoreD= score >= 60 && score <= 69
-
-let scoreE= score >= 50 && score <= 59
-
-let scoreF= score < 50 && score >= 0
-
-let scoreFinal;
-
-if(scoreA){
-    scoreFinal= 'A'
-}else if(scoreB){
-    scoreFinal= 'B'
-}else if(scoreC){
-    scoreFinal= 'C'
-}else if(scoreD){
-    scoreFinal= 'D'
-}else if(scoreE){
-    scoreFinal= 'E'
-}else if(scoreF){
-    scoreFinal= 'F'
-}else{
-    scoreFinal= 'Nota Invalida'
+    return scoreFinal
 }
 
-console.log(scoreFinal)
+console.log(getScore(101))
+console.log(getScore(94))
+console.log(getScore(82))
+console.log(getScore(76))
+console.log(getScore(64))
+console.log(getScore(55))
+console.log(getScore(38))
+console.log(getScore(-1))
