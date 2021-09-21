086afd67d41b201235dabd5d4033ec3245e11dc7
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-21 12:21:57 -0300

    Consolidando o Estudo de Js Parte 4 Buscando e encontrando dados em Array

--- javascript/Js/scripts7.js
+++ javascript/Js/scripts7.js
@@ -140,3 +140,207 @@ try{
 } catch(error){
     console.log(error)
 }
+
+console.log('Buscando e encontrando dados em Array')
+
+const booksByCategory1 = [
+    {
+        category1: "Riqueza",
+        books1: [
+            {
+                title1: "Os segredos da mente milionaria",
+                author1: "T. Harv Eker",
+            },
+            {
+                title2: "O homem mais rico da Babilonia",
+                author2: "George S. Clason",
+            },
+            {
+                title3: "Pai rico, pai pobre",
+                author3: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+];
+
+const booksByCategory2= [
+    {
+        category2: "Inteligencia Emocional",
+        books2: [
+            {
+                title4: "Você e Insubstituivel",
+                author4: "Augusto Cury",
+            },
+            {
+                title5: "Ansiedade – Como enfrentar o mal do seculo",
+                author5: "Augusto Cury",
+            },
+            {
+                title6: "Os 7 habitos das pessoas altamente eficazes",
+                author6: "Stephen R. Covey",
+            },
+        ],
+    },
+];
+
+const totalCategory1= booksByCategory1.length
+
+for(let category1 of booksByCategory1){
+    console.log('Total de livros da categoria', category1.category1)
+    console.log(category1.books1.length)
+}
+
+function countTitle(){
+    let title1= []
+    let title2= []
+    let title3= []
+
+    for(let category1 of booksByCategory1){
+        for(let books1 of booksByCategory1){
+            if(title1. indexOf(books1.title1) == -1){
+                title1.push(books1.title1)
+            }
+        }
+    }
+
+    for(let category1 of booksByCategory1){
+        for(let books1 of booksByCategory1){
+            if(title2. indexOf(books1.title2) == -1){
+                title2.push(books1.title2)
+            }
+        }
+    }
+
+    for(let category1 of booksByCategory1){
+        for(let books1 of booksByCategory1){
+            if(title3. indexOf(books1.title3) == -1){
+                title3.push(books1.title3)
+            }
+        }
+    }
+
+    console.log('Total de titulos', title1.length)
+    console.log('Total de titulos', title2.length)
+    console.log('Total de titulos', title3.length)
+
+}
+
+countTitle();
+
+function countAuthors(){
+    let author4= []
+    let author5= []
+    let author6= []
+
+    for(let category1 of booksByCategory1){
+        for(let books1 of booksByCategory1){
+            if(author1. indexOf(books1.author1) == -1){
+                author1.push(books1.author1)
+            }
+        }
+    }
+
+    for(let category1 of booksByCategory1){
+        for(let books1 of booksByCategory1){
+            if(author2. indexOf(books1.author2) == -1){
+                author2.push(books1.author2)
+            }
+        }
+    }
+
+    for(let category1 of booksByCategory1){
+        for(let books1 of booksByCategory1){
+            if(author3. indexOf(books1.author3) == -1){
+                author3.push(books1.author3)
+            }
+        }
+    }
+
+    console.log('Total de autores', author1.length)
+    console.log('Total de autores', author2.length)
+    console.log('Total de autores', author3.length)
+
+}
+
+countAuthors();
+
+const totalCategory2= booksByCategory2.length
+
+for(let category2 of booksByCategory2){
+    console.log('Total de livros da categoria', category2.category2)
+    console.log(category2.books2.length)
+}
+
+function countTitle(){
+    let title4= []
+    let title5= []
+    let title6= []
+
+    for(let category2 of booksByCategory2){
+        for(let books2 of booksByCategory2){
+            if(title4. indexOf(books2.title4) == -1){
+                title4.push(books2.title4)
+            }
+        }
+    }
+
+    for(let category2 of booksByCategory2){
+        for(let books2 of booksByCategory2){
+            if(title5. indexOf(books2.title5) == -1){
+                title5.push(books2.title5)
+            }
+        }
+    }
+
+    for(let category2 of booksByCategory2){
+        for(let books2 of booksByCategory2){
+            if(title6. indexOf(books2.title6) == -1){
+                title6.push(books2.title6)
+            }
+        }
+    }
+
+    console.log('Total de titulos', title4.length)
+    console.log('Total de titulos', title5.length)
+    console.log('Total de titulos', title6.length)
+
+}
+
+countTitle();
+
+function countAuthors(){
+    let author4= []
+    let author5= []
+    let author6= []
+
+    for(let category2 of booksByCategory2){
+        for(let books2 of booksByCategory2){
+            if(author4. indexOf(books2.author4) == -1){
+                author4.push(books2.author4)
+            }
+        }
+    }
+
+    for(let category2 of booksByCategory2){
+        for(let books2 of booksByCategory2){
+            if(author5. indexOf(books2.author5) == -1){
+                author5.push(books2.author5)
+            }
+        }
+    }
+
+    for(let category2 of booksByCategory2){
+        for(let books2 of booksByCategory2){
+            if(author6. indexOf(books2.author6) == -1){
+                author6.push(books2.author6)
+            }
+        }
+    }
+
+    console.log('Total de autores', author4.length)
+    console.log('Total de autores', author5.length)
+    console.log('Total de autores', author6.length)
+
+}
+
+countAuthors();
