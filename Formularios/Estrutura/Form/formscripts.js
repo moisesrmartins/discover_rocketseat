456df27bb3674ddeea692353ba5a0359ab412cb3
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-25 11:07:26 -0300

    Formularios Parte 1 Estruturas: Estudo de Forms

---
+++ Formularios/Estrutura/Form/formscripts.js
@@ -0,0 +1,20 @@
+console.log('Estudo de Forms')
+
+//<form></form>
+
+console.log('Utilizacao de GET')
+
+//<form action= "" method= "GET"></form>
+
+console.log('Utilizacao de POST')
+
+//<form action= "" method= "POST"></form>
+
+console.log('Utilizacao de VAZIO')
+
+//<form action= "" method= ""></form>
+
+console.log('Aviso de proibicao de uso')
+
+//<form action= "" method= ""> (<form action= "" method= ""></form>) </form>
+//Problema no codigo gerado
