c245a0941bfd29cf5c0b08fe5be8382488b2f971
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-25 13:59:10 -0300

    Formularios Parte 2 Estruturas:Estudo de Fieldsetlegend

---
+++ Formularios/Estrutura/Fieldset Legend/fieldsetlegendscripts.js
@@ -0,0 +1,10 @@
+console.log('Estudo de Fieldsetlegend')
+
+console.log('Utilizacao de fieldset com')
+console.log('Form')
+console.log('Legend')
+console.log('Label')
+console.log('Input')
+console.log('Button')
+
+//Inicio da criacao de um formulario
