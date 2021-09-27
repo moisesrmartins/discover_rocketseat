48b3088c74d431e2c22a68709d39351723a86fbf
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-27 09:11:21 -0300

    Formularios Parte 4 Estruturas: Estudo de Button

---
+++ Formularios/Estrutura/Button/buttonscripts.js
@@ -0,0 +1,15 @@
+console.log('Estudo de Button')
+
+console.log('Utilizacao de varios tipos de button')
+console.log('Utilizacao de type')
+console.log('Submit')
+console.log('reset')
+console.log('button')
+console.log('autofocus')
+console.log('disabled')
+console.log('name')
+console.log('value')
+console.log('form')
+
+//Usar "reset" ira resetar o que ja foi feito, apagando
+//Usar "disabled" deixa o botao desabilitado, impedindo de ser usado
