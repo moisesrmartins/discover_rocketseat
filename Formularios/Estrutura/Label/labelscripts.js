cd2376dbaee339217c4e56f2dd2234b12a759ca2
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-27 07:53:37 -0300

    Formularios Parte 3 Estruturas: Estudo de Label

---
+++ Formularios/Estrutura/Label/labelscripts.js
@@ -0,0 +1,9 @@
+console.log('Estudo de Label')
+
+console.log('Utilizacao de Label com conectores')
+console.log('For')
+console.log('id')
+console.log('input')
+
+//Usar <label></label> é muito melhor que deixar separado em blocos
+//Criacao do formulario, dividindo nome e idade
