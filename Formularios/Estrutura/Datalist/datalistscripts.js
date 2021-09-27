80ff5550856b006c87da743ab44c2e99373a5b6c
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-27 10:29:03 -0300

    Formularios Parte 5 Estruturas: Estudo de Datalist

---
+++ Formularios/Estrutura/Datalist/datalistscripts.js
@@ -0,0 +1,10 @@
+console.log('Estudo de Datalist')
+
+console.log('Utilizacao de Datalist na criacao de perguntas')
+console.log('Utilizacao de label')
+console.log('Utilizacao de form')
+console.log('Utilizacao de button')
+
+//Com o datalist, pode-se criar perguntas
+//Com o label pode-se responder as perguntas
+//Form me deu acesso ao button para poder enviar minha resposta
