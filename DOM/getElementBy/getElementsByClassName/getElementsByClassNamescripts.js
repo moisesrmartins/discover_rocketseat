d0e8135b6ac94545c5bc895cb97b647ecc672fa1
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-29 16:49:55 -0300

    Refacção do getElementByClassName

---
+++ DOM/getElementBy/getElementsByClassName/getElementsByClassNamescripts.js
@@ -0,0 +1,6 @@
+console.log('Estudo de getElementsByClassName')
+
+const element= document.getElementsByClassName('one');
+console.log(element)
+
+//Correcao do erro: "console.log(element)" possuia Aspas, o impedindo que o programa funcione.
