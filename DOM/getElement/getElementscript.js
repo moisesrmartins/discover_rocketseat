b7c739536d48445b470cf8f82ccdbc5690327ca7
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-09-29 14:59:15 -0300

    Estudo de getElementBy: ClassName

--- DOM/getElement/getElementscript.js
+++ DOM/getElement/getElementscript.js
@@ -2,5 +2,10 @@ console.log('Estudo de getElementBy')
 
 console.log('getElementById')
 
-const element= document.getElementById('blog-title')
-console.log('Element')
+const element1= document.getElementById('blog-title')
+console.log('Element1')
+
+console.log('getElementByClassName')
+
+const element2= document.getElementsByClassName('blog-title')
+console.log('Element2')
