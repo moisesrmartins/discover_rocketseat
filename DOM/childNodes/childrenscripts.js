6c22fd7e989d4e156803c4945c1615f62899b26e
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-10-02 10:37:20 -0300

    Estudo de Child

---
+++ DOM/childNodes/childrenscripts.js
@@ -0,0 +1,16 @@
+console.log('Estudo de children')
+
+console.log('childNodes')
+
+const el1= document.querySelector('body')
+console.log(el1.childNodes)
+
+
+console.log('firstElementChild')
+const el2= document.querySelector('body')
+console.log(el2.firstElementChild)
+
+
+console.log('lastElementChild')
+const el3= document.querySelector('body')
+console.log(el3.lastElementChild)
