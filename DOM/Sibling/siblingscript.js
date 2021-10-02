67b11f4e86d348a4da61495f652ea182fb79b2e3
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-10-02 10:58:14 -0300

    Estudo de Sibling

---
+++ DOM/Sibling/siblingscript.js
@@ -0,0 +1,20 @@
+console.log('Estudo de Sibling')
+
+console.log('nextSibling')
+const el1= document.querySelector('body')
+console.log(el1.nextSibling)
+
+
+console.log('nextElementSibling')
+const el2= document.querySelector('body')
+console.log(el2.nextElementSibling)
+
+
+console.log('previousSibling')
+const el3= document.querySelector('body')
+console.log(el3.previousSibling)
+
+
+console.log('previousElementSibling')
+const el4= document.querySelector('body')
+console.log(el4.previousElementSibling)
