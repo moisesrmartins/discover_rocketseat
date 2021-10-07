0bac8f5f4d9c061e604ce65e4ca7443949c976d5
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-10-07 11:34:12 -0300

    Seletores Combinadores

---
+++ css/Seletores combinadores/index.js
@@ -0,0 +1,13 @@
+console.log('Seletores e Coombinadores')
+
+console.log('Seletores')
+//Utilizado pelo "style.css"
+
+console.log('Combinadores')
+//Utilizado pelo "style.css"
+
+console.log('Child Combinator')
+//Utilizado pelo "style.css"
+
+console.log('Sibling Combinator')
+//Utilizado pelo "style.css"
