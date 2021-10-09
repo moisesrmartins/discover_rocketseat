0af09cec60150dc703699577c3671ebea0e88d90
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-10-09 11:25:32 -0300

    Estudo de Pseudo-classes

---
+++ css/Pseudo-classes/index.js
@@ -0,0 +1,10 @@
+console.log('Estudo de Pseudo-classes')
+
+console.log('Utilizacao de first-child')
+console.log('nth-of-type')
+console.log('nth-child')
+console.log('nth-child(odd)')
+console.log('nth-child(even)')
+console.log('hover')
+console.log('focus')
+console.log('disabled')
