dd2ab0b3db53d7299a172170841f96b425725f20
Moisés R. Martins <moisesmartins.box@gmail.com>
2021-10-11 12:51:43 -0300

    Estudo de Fontes

---
+++ css/Fontes/index.js
@@ -0,0 +1,19 @@
+console.log('Estudo de Fontes')
+
+console.log('p1')
+//Utilização de "font-family" e "font-weight"
+
+console.log('p2')
+//Utilização de "font-style" e "font-size"
+
+console.log('p3')
+///Utilização de "font-variant" e "font-stretch"
+
+console.log('p4')
+///Utilização de "letter-spacing", "world-spacing", "line-height" e "text-transform"
+
+console.log('p5')
+///Utilização de "text-decoration" e "text-align"
+
+console.log('p6')
+///Utilização de "text-shadow"
